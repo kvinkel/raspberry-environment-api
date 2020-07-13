4fd6d154d0dd7f3f223ae6640aacb6844890c2cc
kvinkel <kvinkel@hotmail.com>
2020-07-13 15:58:58 +0200

    Add endpoints for live data

--- src/api.py
+++ src/api.py
@@ -8,5 +8,35 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/temperature', methods=['GET'])
+def get_temperature():
+    return 0
+
+
+@app.route('/humidity', method=['GET'])
+def get_humidity():
+    return 0
+
+
+@app.route('/pressure', method=['GET'])
+def get_pressure():
+    return 0
+
+
+@app.route('/tvoc', method=['GET'])
+def get_tvoc():
+    return 0
+
+
+@app.route('/eco2', method=['GET'])
+def get_eco2():
+    return 0
+
+
+@app.route('cpu-temp', method=['GET'])
+def get_cpu_temp():
+    return 0
+
+
 if __name__ == '__main__':
     app.run()
