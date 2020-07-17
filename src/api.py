a6874e86d884b511c94cec2b2447c9b1a9fa0b3c
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-07-17 16:38:27 +0200

    Update flask to run on port 80 & some endpoint fixes

--- src/api.py
+++ src/api.py
@@ -18,8 +18,8 @@ def hello_world():
 @app.route('/sensors', methods=['GET'])
 def get_sensor_values():
     temperature = round(bme280.get_temperature(), 2)
-    humidity = round(bme280.get_humidity, 2)
-    pressure = round(bme280.get_pressure, 2)
+    humidity = round(bme280.get_humidity(), 2)
+    pressure = round(bme280.get_pressure(), 2)
     json = {
         "temperature": temperature,
         "humidity": humidity,
@@ -30,33 +30,33 @@ def get_sensor_values():
 
 @app.route('/temperature', methods=['GET'])
 def get_temperature():
-    return bme280.get_temperature()
+    return str(bme280.get_temperature())
 
 
 @app.route('/humidity', methods=['GET'])
 def get_humidity():
-    return bme280.get_humidity()
+    return str(bme280.get_humidity())
 
 
 @app.route('/pressure', methods=['GET'])
 def get_pressure():
-    return bme280.get_pressure()
+    return str(bme280.get_pressure())
 
 
 @app.route('/tvoc', methods=['GET'])
 def get_tvoc():
-    return 0
+    return '0'
 
 
 @app.route('/eco2', methods=['GET'])
 def get_eco2():
-    return 0
+    return '0'
 
 
-@app.route('cpu-temp', methods=['GET'])
+@app.route('/cpu-temp', methods=['GET'])
 def get_cpu_temp():
-    return 0
+    return '0'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80)
