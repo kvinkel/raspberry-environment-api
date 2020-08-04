2491817eee92d7ac526e7e59aadbe04fb6b120c2
kvinkel <kvinkel@hotmail.com>
2020-08-04 16:43:14 +0200

    Add get_avg_data() and fix query parameters in add_sensor_data() for db

--- src/database.py
+++ src/database.py
@@ -29,5 +29,22 @@ def set_up():
 
 
 def add_sensor_data(temp, hum, pres, tvoc, eco2, cpu_temp):
-    query('INSERT INTO sensor(temperature, humidity, pressure, tvoc, eco2, cpu_temp) VALUES (?, ?, ?, ?, ?, ?);',
-          (temp, hum, pres, tvoc, eco2, cpu_temp))
+    query(
+        'INSERT INTO sensor(temperature, humidity, pressure, tvoc, eco2, cpu_temp) VALUES ('
+        + str(temp) + ',' + str(hum) + ',' + str(pres) + ',' + str(tvoc) + ',' + str(eco2) + ',' + str(cpu_temp) + ')')
+
+
+def get_avg_data():
+    result = query(
+        'SELECT AVG(temperature), AVG(humidity), AVG(pressure), AVG(tvoc), AVG(eco2), AVG(cpu_temp) FROM sensor;')
+    average = {}
+    for row in result:
+        average = {
+            "temperature": row[0],
+            "humidity": row[1],
+            "pressure": row[2],
+            "tvoc": row[3],
+            "eco2": row[4],
+            "cpu_temp": row[5]
+        }
+    return average
