c2c2a93fbfd41a677bf998d030733155bf1e30b4
kvinkel <kvinkel@hotmail.com>
2020-08-04 14:12:50 +0200

    Add database set_up()

--- src/database.py
+++ src/database.py
@@ -1,4 +1,22 @@
 import sqlite3
 
-con = sqlite3.connect('test.db')
 
+def set_up():
+    try:
+        connection = sqlite3.connect('test.db')
+        cursor = connection.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS sensor(id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                       'timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,'
+                       'temperature REAL,'
+                       'humidity REAL,'
+                       'pressure REAL,'
+                       'tvoc INTEGER,'
+                       'eco2 INTEGER,'
+                       'cpu_temp REAL);')
+        connection.commit()
+        cursor.close()
+    except sqlite3.Error as error:
+        print(error)
+    finally:
+        if connection:
+            connection.close()
