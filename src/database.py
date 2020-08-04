f83c75ff68a9e3951d14544a479c80c58916924d
kvinkel <kvinkel@hotmail.com>
2020-08-04 14:41:47 +0200

    Add query(sql) to db and update set_up()

--- src/database.py
+++ src/database.py
@@ -1,22 +1,28 @@
 import sqlite3
 
 
-def set_up():
+def query(sql):
     try:
         connection = sqlite3.connect('test.db')
         cursor = connection.cursor()
-        cursor.execute('CREATE TABLE IF NOT EXISTS sensor(id INTEGER PRIMARY KEY AUTOINCREMENT,'
-                       'timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,'
-                       'temperature REAL,'
-                       'humidity REAL,'
-                       'pressure REAL,'
-                       'tvoc INTEGER,'
-                       'eco2 INTEGER,'
-                       'cpu_temp REAL);')
+        result = cursor.execute(sql).fetchall()
         connection.commit()
         cursor.close()
+        connection.close()
+        return result
     except sqlite3.Error as error:
         print(error)
     finally:
         if connection:
             connection.close()
+
+
+def set_up():
+    query('CREATE TABLE IF NOT EXISTS sensor(id INTEGER PRIMARY KEY AUTOINCREMENT,'
+          'timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,'
+          'temperature REAL,'
+          'humidity REAL,'
+          'pressure REAL,'
+          'tvoc INTEGER,'
+          'eco2 INTEGER,'
+          'cpu_temp REAL);')
