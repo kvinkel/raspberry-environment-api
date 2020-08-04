f3a0fda39717fb92f7b66d091c1cf4019cfab6c6
kvinkel <kvinkel@hotmail.com>
2020-08-04 21:33:23 +0200

    Add get_min_max() and get_measurement_info to db

--- src/database.py
+++ src/database.py
@@ -29,9 +29,8 @@ def set_up():
 
 
 def add_sensor_data(temp, hum, pres, tvoc, eco2, cpu_temp):
-    query(
-        'INSERT INTO sensor(temperature, humidity, pressure, tvoc, eco2, cpu_temp) VALUES ('
-        + str(temp) + ',' + str(hum) + ',' + str(pres) + ',' + str(tvoc) + ',' + str(eco2) + ',' + str(cpu_temp) + ')')
+    query('INSERT INTO sensor(temperature, humidity, pressure, tvoc, eco2, cpu_temp) VALUES (' + str(temp) + ','
+          + str(hum) + ',' + str(pres) + ',' + str(tvoc) + ',' + str(eco2) + ',' + str(cpu_temp) + ');')
 
 
 def get_avg_data():
@@ -48,3 +47,37 @@ def get_avg_data():
             "cpu_temp": row[5]
         }
     return average
+
+
+def get_min_max():
+    result = query(
+        'SELECT MIN(temperature), MIN(humidity), MIN(pressure), MIN(tvoc), MIN(eco2), MIN(cpu_temp),'
+        ' MAX(temperature), MAX(humidity), MAX(pressure), MAX(tvoc), MAX(eco2), MAX(cpu_temp) FROM sensor;')
+    min_max = {}
+    for row in result:
+        min_max = {
+            "min_temp": row[0],
+            "min_hum": row[1],
+            "min_press": row[2],
+            "min_tvoc": row[3],
+            "min_eco2": row[4],
+            "min_cpu_temp": row[5],
+            "max_temp": row[6],
+            "max_hum": row[7],
+            "max_press": row[8],
+            "max_tvoc": row[9],
+            "max_eco2": row[10],
+            "max_cpu_temp": row[11]
+        }
+    return min_max
+
+
+def get_measurement_info():
+    result = query('SELECT MIN(timestamp), COUNT(*) FROM sensor;')
+    info = {}
+    for row in result:
+        info = {
+            "first_measurement": row[0],
+            "total_measurements": row[1]
+        }
+    return info
